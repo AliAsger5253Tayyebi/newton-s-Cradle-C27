919c29dd79ce73aa99d705ffeb5b7e5ae5697d5d
AliAsger5253Tayyebi <76243812+AliAsger5253Tayyebi@users.noreply.github.com>
2021-04-19 17:48:51 +0530

    Add files via upload

---
+++ roof.js
@@ -0,0 +1,32 @@
+class roof
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=x;
+		this.y=y;
+		this.w=w
+		this.h=h
+		this.image = loadImage ("steel rod.png")
+		this.body=Bodies.rectangle(x, y, w, h , options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			imageMode(CENTER)
+			//strokeWeight(4);
+			fill(128,128,128)
+			image(this.image,0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
